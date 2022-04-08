32879574025fc49e1b1572faa3cfc2481b5736dd
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-08 23:47:14 +0300

    added css to dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import classes from "./Dialogs.module.css";
+import {NavLink, Route} from "react-router-dom";
+import Profile from "../Profile/Profile";
 
 const Dialogs = (props: any) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.listOfCompanions}>
-                <div className={classes.companion + ' ' + classes.active}>Aleksandra</div>
-                <div className={classes.companion}>Vladislav</div>
-                <div className={classes.companion}>Ekaterina</div>
+                <div className={classes.companion}>
+                    <NavLink to="/Aleksandra" activeClassName={classes.active}>Aleksandra</NavLink>
+                </div>
+                <div className={classes.companion}>
+                    <NavLink to="/Vladislav" activeClassName={classes.active}>Vladislav</NavLink>
+                </div>
+                <div className={classes.companion}>
+                    <NavLink to="/Ekaterina" activeClassName={classes.active}>Ekaterina</NavLink>
+                </div>
             </div>
             <div className={classes.conversation}>
+         {/*    <Route path="/Aleksandra" component={Dialogs!}/>
+                <Route path="/Vladislav" component={Dialogs!}/>
+                <Route path="/Ekaterina" component={Dialogs!}/>*/}
                 <div className={classes.messageInConversation}>Hello!</div>
                 <div className={classes.messageInConversation}>What's up?</div>
                 <div className={classes.messageInConversation}>What are you gonna do this weekend?</div>
