a31f148dd74f3fee20da4e65188c5a854c383604
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-08 23:26:08 +0300

    added components of menu

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,18 @@ import classes from "./Dialogs.module.css";
 
 const Dialogs = (props: any) => {
     return (
-        <div>
-            DIALOGS
+        <div className={classes.dialogs}>
+            <div className={classes.listOfCompanions}>
+                <div className={classes.companion + ' ' + classes.active}>Aleksandra</div>
+                <div className={classes.companion}>Vladislav</div>
+                <div className={classes.companion}>Ekaterina</div>
+            </div>
+            <div className={classes.conversation}>
+                <div className={classes.messageInConversation}>Hello!</div>
+                <div className={classes.messageInConversation}>What's up?</div>
+                <div className={classes.messageInConversation}>What are you gonna do this weekend?</div>
+                <div className={classes.messageInConversation}>Let's go with us))</div>
+            </div>
         </div>
     );
 };
