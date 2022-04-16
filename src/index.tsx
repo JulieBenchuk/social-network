6867fd0848e5e6f874a0633d27e7bdc890c708b4
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-16 22:24:19 +0300

    moved state.tsx into redux directory

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {state} from "./state";
+import {state} from "./redux/state";
 
 ReactDOM.render(
     <App posts={state.posts} message={state.message} dialog={state.dialog}/>,
