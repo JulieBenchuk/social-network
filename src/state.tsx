c37177974e312a00399fd1213911db7c479306d0
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-16 21:58:49 +0300

    added state.tsx

---
+++ src/state.tsx
@@ -0,0 +1,22 @@
+
+export let state = {
+    postsData: [
+            {id: 1, post: "Hello!", likeCount: 100},
+            {id: 2, post: "How many of us are here!", likeCount: 140},
+            {id: 3, post: "I like this network!", likeCount: 200},
+            {id: 4, post: "Woooow", likeCount: 200}
+        ],
+    messagesData: [
+        {id: 1, message: "Hello!"},
+        {id: 2, message: "How are you?"},
+        {id: 3, message: "Wow! You look great!"},
+        {id: 4, message: "Let's go out)"},
+        {id: 5, message: "Nice to meet you!"},
+        {id: 6, message: "I'm Julie."}
+
+    ],
+    dialogsData: [
+        {id: 1, name: "Aleksandra"},
+        {id: 2, name: "Vladislav"},
+        {id: 3, name: "Veronika"}
+    ]}
