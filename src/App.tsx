e09346d7fd33d41cebf93ac9a3af8898018a4f8a
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-23 23:05:38 +0300

    add state props type

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,24 @@ import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Route} from "react-router-dom";
-import {PostPropsType} from "./components/Profile/MyPosts/Post/Post";
-import {MessagePropsType} from "./components/Dialogs/Message/Message";
-import {DialogItemPropsType} from "./components/Dialogs/DialogItem/DialogItem";
+import {Route} from "react-router-dom";
+import {state} from "./redux/state";
 
-type AppPropsType = {
-    state: any
-}
-const App = (props: AppPropsType) => {
+
+
+const App = () => {
     return (
-        <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Nav_bar/>
                 <div className="app-wrapper-content">
-                    <Route path="/profile" render={() => <Profile post={props.state.profilePage.posts}/>}/>
-                    <Route path="/messages" render={() => <Dialogs dialog={props.state.dialogsPage.dialogs} message={props.state.dialogsPage.messages}/>}/>
-                    <Route path="/news" render={() => <News/>}/>
-                    <Route path="/music" render={() => <Music/>}/>
-                    <Route path="/settings" render={() => <Settings/>}/>
+                    <Route path={"/profile"} render={() => <Profile post={state.profilePage.posts}/>}/>
+                    <Route path={"/messages"} render={() => <Dialogs dialog={state.dialogsPage.dialogs} message={state.dialogsPage.messages}/>}/>
+                    <Route path={"/news"} render={() => <News/>}/>
+                    <Route path={"/music"} render={() => <Music/>}/>
+                    <Route path={"/settings"} render={() => <Settings/>}/>
                 </div>
             </div>
-        </BrowserRouter>
     );
 }
 
