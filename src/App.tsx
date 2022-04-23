3182f02f3d6273572fa329d4ce57e8b0140a2097
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-23 22:02:39 +0300

    add state props type

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,32 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Nav_bar from "./components/Nav_bar/Nav_bar";
 import Dialogs from "./components/Dialogs/Dialogs";
-import Profile from "./components/Profile/Profile";
+import Profile, {ProfilePropsType} from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
-import {PostPropsType} from "./components/Profile/MyPosts/Post/Post";
-import {MessagePropsType} from "./components/Dialogs/Message/Message";
-import {DialogItemPropsType} from "./components/Dialogs/DialogItem/DialogItem";
 
-type AppPropsType = {
-    state: any
+type StatePropsType = {
+    profilePage: any // ???????
+    dialogsPage: any // ???????
+
 }
-const App = (props: AppPropsType) => {
+type AppStatePropsType = {
+    state: StatePropsType
+}
+const App = (props: AppStatePropsType) => {
     return (
         <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Nav_bar/>
                 <div className="app-wrapper-content">
-                    <Route path="/profile" render={() => <Profile post={props.state.profilePage.posts}/>}/>
-                    <Route path="/messages" render={() => <Dialogs dialog={props.state.dialogsPage.dialogs} message={props.state.dialogsPage.messages}/>}/>
-                    <Route path="/news" render={() => <News/>}/>
-                    <Route path="/music" render={() => <Music/>}/>
-                    <Route path="/settings" render={() => <Settings/>}/>
+                    <Route path={"/profile"} render={() => <Profile post={props.state.profilePage.posts}/>}/>
+                    <Route path={"/messages"} render={() => <Dialogs dialog={props.state.dialogsPage.dialogs} message={props.state.dialogsPage.messages}/>}/>
+                    <Route path={"/news"} render={() => <News/>}/>
+                    <Route path={"/music"} render={() => <Music/>}/>
+                    <Route path={"/settings"} render={() => <Settings/>}/>
                 </div>
             </div>
         </BrowserRouter>
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,4 +1,31 @@
-export let state = {
+type PostType = {
+    id: number
+    post: string
+    likeCount: number
+}
+type ProfilePageType = {
+    posts: Array<PostType>
+}
+type MessageType = {
+    id: number
+    message: string
+}
+type DialogType = {
+    id: number
+    name: string
+    avatar: string
+}
+type DialogsPageType = {
+    messages: Array<MessageType>
+    dialogs: Array<DialogType>
+}
+type RootStateType = {
+    profilePage: ProfilePageType
+    dialogsPage: DialogsPageType
+}
+
+
+export let state: RootStateType = {
     profilePage: {
         posts: [
             {id: 1, post: "Hello!", likeCount: 100},
@@ -18,9 +45,21 @@ export let state = {
 
         ],
         dialogs: [
-            {id: 1, name: "Aleksandra", avatar: "https://image.shutterstock.com/z/stock-vector-portrait-of-an-asian-girl-with-pink-hair-avatar-for-a-social-network-subcultures-anime-and-1397661953.jpg"},
-            {id: 2, name: "Vladislav", avatar: "https://static.vecteezy.com/system/resources/previews/002/275/817/large_2x/asian-female-avatar-woman-icon-for-network-vector.jpg"},
-            {id: 3, name: "Veronika", avatar: "https://as1.ftcdn.net/v2/jpg/02/85/98/20/1000_F_285982046_zzxKDt4O2ntMLBObfqU2bdEovgRclEqa.jpg"}
+            {
+                id: 1,
+                name: "Aleksandra",
+                avatar: "https://image.shutterstock.com/z/stock-vector-portrait-of-an-asian-girl-with-pink-hair-avatar-for-a-social-network-subcultures-anime-and-1397661953.jpg"
+            },
+            {
+                id: 2,
+                name: "Vladislav",
+                avatar: "https://static.vecteezy.com/system/resources/previews/002/275/817/large_2x/asian-female-avatar-woman-icon-for-network-vector.jpg"
+            },
+            {
+                id: 3,
+                name: "Veronika",
+                avatar: "https://as1.ftcdn.net/v2/jpg/02/85/98/20/1000_F_285982046_zzxKDt4O2ntMLBObfqU2bdEovgRclEqa.jpg"
+            }
         ]
     }
 }
