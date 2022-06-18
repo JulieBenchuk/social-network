236d701b6510dff388d495a85ef404d934b08b20
JulieBenchuk <juliebenchuk@gmail.com>
2022-06-18 23:07:30 +0300

    added type of withRouter

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -4,17 +4,30 @@ import axios from "axios";
 import {connect} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {setUserProfile} from "../../redux/profile-reducer";
-import {withRouter} from "react-router-dom";
+import {RouteComponentProps, withRouter} from "react-router-dom";
+import profile from "./Profile";
 
-let mapStateToProps = (state: AppStateType) => ({
+type MapStateToPropsType = {
+    profile: any
+}
+type MapDispatchToPropsType = {
+    setUserProfile: (profile: any)=> void
+}
+type OwnPropsType = MapStateToPropsType & MapDispatchToPropsType
+type PathParamsType = {
+    userID: string
+}
+type PropsType = RouteComponentProps<PathParamsType>&OwnPropsType
+
+let mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
     profile: state.profilePage.profile
 })
 
-class ProfileContainer extends React.Component<any>{
+class ProfileContainer extends React.Component<PropsType>{
     componentDidMount() {
         let userID = this.props.match.params.userID
         if (!userID){
-            userID=24112
+            userID="24112"
         }
         axios.get("https://social-network.samuraijs.com/api/1.0/profile/" + userID).then(response => {
             this.props.setUserProfile(response.data)
