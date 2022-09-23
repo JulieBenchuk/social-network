d272f59b64c76e504a01d4704669273c4b1f4989
JulieBenchuk <juliebenchuk@gmail.com>
2022-09-23 22:03:37 +0300

    Updates

---
+++ static/js/610.ec9ab0f0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksamurai_way=self.webpackChunksamurai_way||[]).push([[610],{610:function(a,u,e){e.r(u);e(2791);var n=e(184);u.default=function(){return(0,n.jsx)("div",{children:"NEWS"})}}}]);
+//# sourceMappingURL=610.ec9ab0f0.chunk.js.map
---
+++ static/js/739.cbb0fbba.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksamurai_way=self.webpackChunksamurai_way||[]).push([[739],{1535:function(a,u,e){e.r(u);e(2791);var n=e(184);u.default=function(){return(0,n.jsx)("div",{children:"MUSIC"})}}}]);
+//# sourceMappingURL=739.cbb0fbba.chunk.js.map
