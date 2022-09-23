c98dcd1297ab0011aff263737fa9381e91abfc90
JulieBenchuk <juliebenchuk@gmail.com>
2022-09-23 22:36:38 +0300

    refactor: api key changed

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: `https://social-network.samuraijs.com/api/1.0/`,
     withCredentials: true,
     headers: {
-        "API-KEY": "25e2877e-c600-4999-b524-1694f1ae4252"
+        "API-KEY": "551f2e1a-d9fe-4d4c-817c-0a0c51d747e02"
     }
 })
 
