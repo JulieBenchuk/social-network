bb9941f71ed09367ccf78ea32875249545b9d844
JulieBenchuk <juliebenchuk@gmail.com>
2022-09-24 00:12:41 +0300

    refactor: added HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import React from "react";
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
@@ -7,10 +7,10 @@ import App from "./App";
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
-            <App />
+            <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
