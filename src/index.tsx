65f1d96edf03160f62d563f2a2143fe94ff3dbc6
JulieBenchuk <juliebenchuk@gmail.com>
2022-09-24 18:55:57 +0300

    fix: gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import React from "react";
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
