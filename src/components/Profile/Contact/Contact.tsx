5d68b53744e4542a1d4f961d9955534d71f157f0
JulieBenchuk <juliebenchuk@gmail.com>
2022-09-25 15:19:33 +0300

    feat: added contact component

---
+++ src/components/Profile/Contact/Contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from "./Contact.module.css"
+
+type ContactPropsType = {
+    contactTitle: string
+    contactValue: string
+}
+export const Contact: React.FC<ContactPropsType> = ({contactTitle, contactValue}) => {
+    return (
+        <div className={s.contact}>
+            <p className={s.title}>{contactTitle}:</p>
+            <p className={s.value}>{contactValue}</p>
+        </div>
+    );
+};
