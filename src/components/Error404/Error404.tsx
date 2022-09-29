1d12aff060b76e071d42e7c991c7bcc67bbea0d4
JulieBenchuk <juliebenchuk@gmail.com>
2022-09-29 22:51:44 +0300

    feat: 404 css, img

--- src/components/Error404/Error404.tsx
+++ src/components/Error404/Error404.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from "./Error404.module.css"
+import img404 from "./../../assets/img/error404.jpeg"
 
 export const Error404
 = () => {
     return (
         <div className={s.error_block}>
-            Error 404
+            <img src={img404} alt="404_not_found"/>
         </div>
     );
 };
