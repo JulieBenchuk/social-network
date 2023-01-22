284fd990752ea51e138466936a919b331ad48166
JulieBenchuk <juliebenchuk@gmail.com>
2023-01-22 13:59:21 +0300

    refactor: added icons to ProfileDataForm and changed css

--- src/components/Profile/ProfileInfo/ProfileDataForm/ProfileDataForm.tsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm/ProfileDataForm.tsx
@@ -3,6 +3,9 @@ import {Field, InjectedFormProps, reduxForm} from "redux-form";
 import {Input, Textarea} from "../../../../common/Forms-control/FormsControl";
 import {ContactsType} from "../../../../api/api";
 import s from "./ProfileDataForm.module.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAddressCard, faCircleCheck} from "@fortawesome/free-regular-svg-icons";
+import {faLink, faMagnifyingGlass, faPerson, faSave} from "@fortawesome/free-solid-svg-icons";
 
 export type ProfileDataFormDataType = {
     fullName: string
@@ -17,41 +20,54 @@ export const ProfileDataForm: React.FC<InjectedFormProps<ProfileDataFormDataType
                                                                                           initialValues, error
                                                                                       }) => {
     return (
-        <form onSubmit={handleSubmit}>
+        <div className={s.profileDataForm}>
+            <form onSubmit={handleSubmit}>
 
-            <div>
-                <button>Save</button>
-            </div>
+                    <div className={s.dataItem}>
+                        <FontAwesomeIcon icon={faPerson}/>
+                        <b> Full name: </b>
+                        <Field placeholder="full name" name={"fullName"} component={Input}/>
+                    </div>
 
-            <div>
-                <b>Full name: </b>
-                <Field placeholder="full name" name={"fullName"} component={Input}/>
-                <div>
-                    <b>About me: </b>
-                    <Field placeholder="about me" name={"aboutMe"} component={Textarea}/>
-                </div>
-                <div>
-                    <b>Looking job: </b>
-                    <Field name={"lookingForAJob"} component={Input} type={"checkbox"}/>
-                </div>
-                <div>
-                    <b>My skills: </b>
-                    <Field placeholder="my skills" name={"lookingForAJobDescription"} component={Textarea}/>
-                </div>
+                    <div className={s.dataItem}>
+                        <FontAwesomeIcon icon={faAddressCard}/>
+                        <b> About me: </b>
+                        <Field placeholder="about me" name={"aboutMe"} component={Textarea} />
+                    </div>
 
-                <div>
-                    <b>Contacts: </b>
+                    <div className={s.dataItem}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                        <b> Looking for a job: </b>
+                        <Field name={"lookingForAJob"} component={Input} type={"checkbox"}/>
+                    </div>
 
-                    {initialValues.contacts && Object.keys(initialValues.contacts).map(key => {
-                        return <div key={key}>
-                            <b>{key}:</b>
-                            <Field placeholder={key} name={"contacts." + key} component={Input}/>
-                            {error && <div className={s.error}>{error}</div>}
+                    <div className={s.dataItem}>
+                        <FontAwesomeIcon icon={faCircleCheck}/>
+                        <b> My skills: </b>
+                        <Field placeholder="my skills" name={"lookingForAJobDescription"} component={Textarea}/>
+                    </div>
+
+                    <div className={s.dataItem}>
+                        <FontAwesomeIcon icon={faLink}/>
+                        <b> Contacts: </b>
+
+                        <div className={s.contacts}>
+                            {initialValues.contacts && Object.keys(initialValues.contacts).map(key => {
+                                return <div key={key}>
+                                    <b>{key}:</b>
+                                    <Field placeholder={key} name={"contacts." + key} component={Input}/>
+                                    {error && <div className={s.error}>{error}</div>}
+                                </div>
+                            })}
                         </div>
-                    })}
+
+                    </div>
+
+                <div>
+                    <button><FontAwesomeIcon icon={faSave}/> Save</button>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     );
 };
 
