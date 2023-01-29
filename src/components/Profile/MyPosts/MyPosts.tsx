0ffad079ae05ad0302badc1b18cd0358c7d0b27b
JulieBenchuk <juliebenchuk@gmail.com>
2023-01-29 23:17:06 +0300

    feat: simulate adding a new post to the state (current posts are received from state randomly)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from "react";
+import React, {useMemo, useState} from "react";
 import style from "./MyPosts.module.css";
 import Post from "./Post/Post";
 import {MyPostsPropsType} from "./MyPostsContainer";
@@ -7,17 +7,23 @@ import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../../common/Forms-control/FormsControl";
 import {SuperButton} from "../../../common/SuperButton/SuperButton";
 import {getArrayWithRandomNumber} from "../../../common/utils/getArrayWithRandomNumber";
+import {PostType} from "../../../redux/profile-reducer";
 
 const maxLengthCreator30 = maxLengthCreator(30);
 
-const MyPosts: React.FC<MyPostsPropsType> = ({addPost, posts, likePost, profilePhoto, ...restProps}) => {
+const MyPosts: React.FC<MyPostsPropsType> = ({addPost, posts, likePost, profilePhoto}) => {
+
+    /// simulate adding a new post to the state (current posts are received from state randomly)
+    const [newPostArray, setNewPostArray] = useState<Array<PostType>>([])
 
     const onAddPost = (values: any) => {
-        addPost(values.newPostText);
-
+        addPost(values.newPostText)
+        newPostArray.unshift({id: Math.ceil(Math.random()*10000), post: values.newPostText, likeCount: 0})
+        setNewPostArray(newPostArray)
     }
     const onLikePost = (postID: number, likeCount: number) => {
         likePost(postID, likeCount)
+        newPostArray.map(p=>p.id===postID ? p.likeCount++ : p)
     }
 
     const randomNumber = useMemo(() => {
@@ -26,8 +32,8 @@ const MyPosts: React.FC<MyPostsPropsType> = ({addPost, posts, likePost, profileP
 
 
     const filteredRandomPosts = useMemo(() => {
-        return posts.filter(p => randomNumber.includes(p.id))
-    }, [posts])
+        return newPostArray.concat(posts.filter(p => randomNumber.includes(p.id)))
+    }, [posts, newPostArray])
 
 
     const postElements = filteredRandomPosts.map((p, index) => (
